40a171015b097fb948d37ebf321c56fb5e0f3a1e
Vertexwahn <julian.amann@tum.de>
2023-02-08 21:41:01 +0100

    Update Hello World demos

--- hello_world/Go/main.go
+++ hello_world/Go/main.go
@@ -1,11 +1,12 @@
+package main
+
 /*
  *  SPDX-FileCopyrightText: 2022 Julian Amann <dev@vertexwahn.de>
  *  SPDX-License-Identifier: Apache-2.0
  */
 
-package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello World from Go!")
+	fmt.Println("Hello World from Go!")
 }
