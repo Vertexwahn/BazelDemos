0451f718e03de7c7f1c931aae9887f37d1c4d6bb
Vertexwahn <julian.amann@tum.de>
2024-01-21 22:14:15 +0100

    Project import generated by Copybara.
    GitOrigin-RevId: 909c2c5cb9f5e6b3efc53be9d870d04cdb944755

--- hello_world/go/main.go
+++ hello_world/go/main.go
@@ -1,12 +1,14 @@
 package main
 
 /*
- *  SPDX-FileCopyrightText: 2022 Julian Amann <dev@vertexwahn.de>
+ *  SPDX-FileCopyrightText: 2022-2024 Julian Amann <dev@vertexwahn.de>
  *  SPDX-License-Identifier: Apache-2.0
  */
 
 import "fmt"
+import "runtime"
 
 func main() {
 	fmt.Println("Hello World from Go!")
+	fmt.Printf("Go version: %s\n", runtime.Version())
 }
