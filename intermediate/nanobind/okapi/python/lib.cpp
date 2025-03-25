0b2a186ddd8c260c83683198b7ec4cce59ff1af6
Vertexwahn <julian.amann@tum.de>
2025-03-25 10:28:00 +0100

    Project import generated by Copybara.
    GitOrigin-RevId: 6563375178c810720985eb75d69619d060250d36

--- intermediate/nanobind/okapi/python/lib.cpp
+++ intermediate/nanobind/okapi/python/lib.cpp
@@ -2,12 +2,15 @@
 #include "nanobind/stl/string.h"
 
 #include <string>
+#include <vector>
 
 namespace nb = nanobind;
 
 using namespace nb::literals;
 
 namespace okapi {
+    constexpr float LAMBDA_MIN = 360, LAMBDA_MAX = 830;
+
     struct Spectrum {
         virtual float evaluate(float lambda) const = 0;
     };
@@ -20,16 +23,33 @@ namespace okapi {
     private:
         float value;
     };
+
+    class DenselySampledSpectrum : Spectrum {
+    public:
+        DenselySampledSpectrum(const Spectrum& spectrum) {
+            for(int i = LAMBDA_MIN; i <= LAMBDA_MAX; i++) {
+                values.emplace_back(spectrum.evaluate(i));
+            }
+        }
+        float evaluate(float lambda) const override {
+            return values[lambda - LAMBDA_MIN];
+        }
+
+    private:
+        std::vector<float> values;
+    };
 }
 
 using namespace okapi;
 
 NB_MODULE(okapi, m) {
-    
     nb::class_<Spectrum>(m, "Spectrum")
         .def("evaluate", &Spectrum::evaluate);
   
     nb::class_<ConstantSpectrum, Spectrum>(m, "ConstantSpectrum")
         .def(nb::init<float>())
         .def("evaluate", &ConstantSpectrum::evaluate);
+
+    nb::class_<DenselySampledSpectrum, Spectrum>(m, "DenselySampledSpectrum")
+        .def("evaluate", &DenselySampledSpectrum::evaluate);
 }
